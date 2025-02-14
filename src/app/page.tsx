8b82c9f536ca9d238ca770e543acafeed060d870
felipecavalcantecapim <felipe.cavalcante@capimtecnologia.com.br>
2025-02-14 12:05:41 -0300

    adição de nova rota Contra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 // import Footer from '@/app/components/Footer';
 import Header from '@/app/components/Header';
 // import Hero from '@/app/components/Hero';
+// import Contra from '@/app/components/Contra';
 // import Insta from '@/app/components/Insta';
 // import Contact from '@/app/components/Contact';
 // import Projects from '@/app/components/Projects';
@@ -13,6 +14,7 @@ export default function Home() {
     <main className='relative text-2xl text-red-900 bg-white'>
       <Header />
       {/* <Hero /> */}
+      {/* <Contra /> */}
       {/* <Us /> */}
       {/* <Services /> */}
       {/* <Packs /> */}
